46b3a0b02fedd1e2e8ae6eb507c59df97582d7ca
Matthew Summers <matt@wishf.co.uk>
2012-06-03 21:34:19 +0100

    Fixed AssemblyInfo.cs
    Left traces of alternate name, added copyright data

--- OptiPNGFrontend/Properties/AssemblyInfo.cs
+++ OptiPNGFrontend/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("Unicodify")]
+[assembly: AssemblyTitle("OptiPNGFrontend")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Unicodify")]
-[assembly: AssemblyCopyright("Copyright ©  2012")]
+[assembly: AssemblyProduct("OptiPNGFrontend")]
+[assembly: AssemblyCopyright("Copyright © Matthew Summers 2012")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
